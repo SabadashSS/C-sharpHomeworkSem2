5d65d4a98ad13c9393d10ec7b6032250c7675e05
zaEp <istratkinss@mail.ru>
2022-07-04 16:36:38 +0300

    rdy task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,4 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-Console.WriteLine("Привет \nмир");
+
+namespace HelloWorld 
+{
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            var str = number.ToString();
+            if (99 < number)
+            {
+            Console.WriteLine($"Третья цифра:{str[2]}");
+            }
+            else
+            {
+                Console.WriteLine($"Третьей цифры в числе {str} нет.");
+            }
+
+        } 
+    } 
+}
