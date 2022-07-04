cfc69c4856672477d339f1e6db90433df90f5b1c
zaEp <istratkinss@mail.ru>
2022-07-04 04:05:12 +0300

    add task10

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+namespace HelloWorld 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            string str = Convert.ToString(Console.ReadLine());
+            
+            Console.WriteLine($"{str[1]}");
+        } 
+    } 
+}
